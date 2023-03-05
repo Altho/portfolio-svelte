980f03e5d99de08688e72f5e61c8cfe1c6ce6e59
Altho <alan.at.thomas@gmail.com>
2023-03-05 14:27:29 +0100

    added avatar

--- src/components/Welcome.svelte
+++ src/components/Welcome.svelte
@@ -16,24 +16,32 @@
   }}
     >
         {#if isInView}
-          <div class="content" transition:fade={{duration: 1000 }}>
-              <p class="main">Hello, I am Altho</p>
-              <p>I like to solve problems</p>
-                </div>
+            <div class="content" transition:fade={{duration: 1000 }}>
+                <p class="main">Hello, I am Altho</p>
+                <p>I like to solve problems</p>
+            </div>
+            <div class="avatar"><img src="/images/avatar.webp" alt="avatar" /></div>
         {/if}
     </div>
 </section>
 
-<style>
+<style lang="scss">
     .container {
         width: 100%;
         height: 100vh;
         padding: 20px;
         display: flex;
         font-family: "Exo 2", sans-serif;
-        flex-direction: column;
+
         justify-content: center;
         align-items: center;
+
+      .avatar {
+        img {
+          height: 70vh;
+          padding-left: 20px;
+        }
+      }
 
     }
 
@@ -55,6 +63,17 @@
 
         }
 
+        .avatar {
+            img {
+                height: 70vh;
+                position: absolute;
+                top: 25%;
+                left: 15%;
+                z-index: -1;
+            }
+        }
+
 
     }
 </style>
+
